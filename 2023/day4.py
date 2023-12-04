3c8a85ebf0582116cac122be65614778c5420c34
emnordi <33757302+emnordi@users.noreply.github.com>
2023-12-04 09:49:43 +0100

    Add files via upload

---
+++ 2023/day4.py
@@ -0,0 +1,25 @@
+import numpy as np
+def get_data(fname):
+    with open(fname) as f:
+        data = f.readlines()
+    return data
+
+lines = get_data('task.txt')
+lines = [x.rstrip() for x in lines]
+
+total = 0
+all_cards = np.zeros(len(lines))
+all_cards += 1
+for i, line in enumerate(lines):
+    cardno, numbers = line.split(': ')
+    tickets, results = numbers.split(' | ')
+    tickets = list(filter(None, tickets.split(' ')))
+    results = list(filter(None, results.split(' ')))
+
+    correct_nums = len(list(set(tickets) & set(results)))
+    all_cards[i+1: i + correct_nums + 1] += all_cards[i] * 1
+    if correct_nums > 0:
+        total +=  pow(2,correct_nums - 1)
+
+print('Task 1:', total)
+print('Task 2:', sum(all_cards))
